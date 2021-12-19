11262139a675a94c4ca81ad8d020b947c3c31e9f
Ichiken <ichiken.usa@gmail.com>
2021-12-19 13:44:31 -0800

    ユーザsignup入力チェックのリファクタリング #52

--- Web/signup.php
+++ Web/signup.php
@@ -25,37 +25,25 @@ try {
         // バリデーション
         $err = array();
 
-        // ユーザーID入力なし（HTML側でも制限してるが一応）
-        if (!$user_num) {
-            $err['user_num'] = 'Please input User ID';
-            // ユーザーID長すぎ
-        } elseif (mb_strlen($user_num, 'utf-8') > 12) {
-            $err['user_num'] = 'User ID is too long';
-            // ユーザーID短すぎ
-        } elseif (mb_strlen($user_num, 'utf-8') < 4) {
-            $err['user_num'] = 'User ID is too short';
+        // ユーザ名入力チェック
+        $error_message = null;
+        $error_message = check_length($user_num, 4, 12, 'User ID');
+        if (!empty($error_message)){
+            $err['user_num'] = $error_message;
         }
 
-        // 名前入力確認
-        if (!$name) {
-            $err['name'] = 'Please input Name';
-            // 名前長すぎ
-        } elseif (mb_strlen($name, 'utf-8') > 32) {
-            $err['name'] = 'Name is too long';
-            // 名前短すぎ
-        } elseif (mb_strlen($name, 'utf-8') < 2) {
-            $err['name'] = 'Name is too short';
+        // 名前入力チェック
+        $error_message = null;
+        $error_message = check_length($name, 2, 32, 'User Name');
+        if (!empty($error_message)){
+            $err['name'] = $error_message;
         }
 
         // パスワード入力チェック
-        if (!$password) {
-            $err['password'] = 'Please input Password';
-        } elseif (mb_strlen($password, 'utf-8') > 24) {
-            // パスワード長すぎチェック
-            $err['password'] = 'Password is too long';
-        } elseif (mb_strlen($password, 'utf-8') < 4) {
-            // パスワード短すぎチェック
-            $err['password'] = 'Password is too short';
+        $error_message = null;
+        $error_message = check_length($password, 4, 24, 'Password');
+        if (!empty($error_message)){
+            $err['password'] = $error_message;
         }
 
         ////
